8d07c805d106c349769eef0bdf31aa9ba9cf6683
Didier Brun <db@brundidier.com>
2025-06-07 22:08:16 +0200

    Cors POlicy

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
 export default async ({req, res, log, error}) => {
-    return res.send("Hello World")
+    res.headers = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization, X-Requested-With',
+        'Access-Control-Allow-Credentials': 'false' // Doit être false quand Origin est *
+    };
+
+    // Gérer les requêtes preflight OPTIONS
+    if (req.method === 'OPTIONS') {
+        return res.send('', 200);
+    }
+
+    return res.send("Hello World");
 }
